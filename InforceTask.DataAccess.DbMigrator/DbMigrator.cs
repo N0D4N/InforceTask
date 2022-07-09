ea5f17288470bc39ed162b1d923ca2af102be7e4
N0D4N <50947930+N0D4N@users.noreply.github.com>
2022-07-09 17:24:10 +0300

    Update DbMigrator.cs

--- InforceTask.DataAccess.DbMigrator/DbMigrator.cs
+++ InforceTask.DataAccess.DbMigrator/DbMigrator.cs
@@ -18,8 +18,7 @@ public static class DbMigrator
 				await appDbContext.AboutPageContents.AddAsync(new AboutPageContents
 				{
 					Content =
-						@"Unfortunately i didn't have enough time to finish task completely. Admin still can't edit about page content, though it's dynamically taken from server
-All sync and not async calls to DbContext are intended since SQLite (provider for this task) doesn't support async and it would only result in worse performance.
+						@"First about page content. Click to edit it while logged in as admin user.
 Algo for generating shortened url is pretty simple: create new guid, get a slice of it's string representation, ensure that there isn't alredy such shortcut in DB and you're done.",
 					LastEditDateTime = DateTime.UtcNow
 				});
